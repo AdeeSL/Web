e211f9410c3d033b0d8e2ff9de08939c50225369
AdeeSL <49285003+AdeeSL@users.noreply.github.com>
2020-01-07 13:21:07 +0530

    add home.html & main.css

---
+++ web/home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Home</title>
+</head>
+<body>
+    <div class="social">
+		<a href="#" class="facebook"><i class="fa fa-facebook"></i></a> 
+		<a href="#" class="twitter"><i class="fa fa-twitter"></i></a> 
+		<a href="#" class="google"><i class="fa fa-google"></i></a> 
+		<a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+		<a href="#" class="youtube"><i class="fa fa-youtube"></i></a> 
+	</div>
+
+    <div class="icon-bar">
+		<a class="active"><i class="fa fa-bars"></i> Menu</a>
+		<a href="#"><i class="fa fa-home"></i> Home</a>
+		<a href="#"><i class="fa fa-shopping-cart"></i> Buy</a>
+		<a href="#"><i class="fa fa-phone"></i> Contact</a> 
+		<a href="login.php"><i class="fa fa-user"></i> Login</a> 
+		<a href="#"><i class="fa fa-search"></i> Search</a>
+	</div>
+    
+</body>
+</html>
